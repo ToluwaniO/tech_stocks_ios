cac252a4c5c1ee72e8598306e6a0fa736610a777
toluwani <ogunsanyatoluwani@gmail.com>
2020-07-04 01:29:22 -0400

    Initial Commit

---
+++ Tech Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tech Stocks
+//
+//  Created by Toluwani Ogunsanya on 2020-07-04.
+//  Copyright © 2020 Toluwani Ogunsanya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
